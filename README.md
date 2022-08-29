66355813981f6e776ec09e76426d36185e101101
Klaus Keller <klaus.keller@sap.com>
2022-08-29 16:29:36 -0700

    feat: enable pnpm commands on root: build, lint, test

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # fiori-monorepo
 POC for Fiori apps in a monorepo
+
+
+## Usage
+
+Clone this repository and switch to it
+```shell
+git clone https://github.com/Klaus-Keller/fiori-monorepo.git
+cd fiori-monorepo
+```
+
+Run `pnpm` commands on root level
+```shell
+pnpm install
+pnpm build
+pnpm test
+```
--- packages/sap.demo.fpmeasyuiapp/karma.conf.js
+++ packages/sap.demo.fpmeasyuiapp/karma.conf.js
@@ -2,6 +2,14 @@ module.exports = function (config) {
     "use strict";
 
     config.set({
+        // Make Karma work with pnpm.
+        // See: https://github.com/pnpm/pnpm/issues/720#issuecomment-954120387
+        plugins: Object.keys(require('./package').devDependencies).flatMap(
+            (packageName) => {
+                if (!packageName.startsWith('karma-')) return []
+                return [require(packageName)]
+            }
+        ),
         frameworks: ["ui5"],
         ui5: {
             type: "application",
